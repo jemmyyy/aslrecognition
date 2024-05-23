17bc3c43868c85b271c9907a0c0852943346dc91
Ahmed M. Gamal <amgamal200111@gmail.com>
2024-05-23 19:22:02 +0300

    changing class list

--- api/camera/utils.py
+++ api/camera/utils.py
@@ -1,3 +1,3 @@
 SEQUENCE_LENGTH = 20
-CLASSES_LIST = ["0007", "0008", "0012", "0013", "0014", "0015", "0016", "0017", "0018", "0019"]
-CLASSES_NAMES = ["esmak", "23", "share3", "go", "want", "visit", "good", "eng", "el7", "salam"]
+CLASSES_LIST = ["0161", "0162", "0163", "0164"]
+CLASSES_NAMES = ["drink", "sleep", "wake up", "hear"]
